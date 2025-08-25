a1970c3e0f75d00ae153b947c3260b67e15129b2
Korya25 <????? ??? GitHub>
2025-08-26 00:16:21 +0300

    Integrate Lottie animation in EmptyStateWidget for enhanced user feedback, add new infoLottie asset, and enable DevicePreview in main.dart for development convenience.

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,4 +1,5 @@
 class AppAssets {
   static const String loaderLottieInfinit = 'assets/lottie/Loader 4.json';
   static const String loaderLottie = 'assets/lottie/Loading Dots Blue.json';
+  static const String infoLottie = 'assets/lottie/attention.json';
 }
--- lib/features/car_register/presentation/widgets/empty_state_widget.dart
+++ lib/features/car_register/presentation/widgets/empty_state_widget.dart
@@ -1,8 +1,10 @@
+import 'package:car_register_app/core/constants/app_assets.dart';
 import 'package:car_register_app/core/resources/theme/app_colors.dart';
 import 'package:car_register_app/core/widgets/animations/animate_do.dart';
 import 'package:car_register_app/core/widgets/common/text_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
 
 class EmptyStateWidget extends StatelessWidget {
   const EmptyStateWidget({super.key});
@@ -11,30 +13,31 @@ class EmptyStateWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomFadeInUp(
       duration: 800,
-      child: SizedBox(
-        height: 300,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.warning, size: 80, color: AppColors.primary),
-              SizedBox(height: 20.h),
-              TextApp(
-                text: 'لا توجد لوحات مسجلة',
-                type: TextAppType.bodyLarge,
-                color: AppColors.textAndIconPrimary,
-                fontWeight: FontWeight.w600,
-                fontSize: 20,
-              ),
-              SizedBox(height: 12.h),
-              TextApp(
-                text: 'ابدأ بإضافة رقم اللوحه ',
-                type: TextAppType.bodyMedium,
-                color: AppColors.textAndIconSecondary,
-                fontSize: 16,
-              ),
-            ],
-          ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              AppAssets.infoLottie,
+              height: 0.15.sh,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20.h),
+            TextApp(
+              text: 'لا توجد لوحات مسجلة',
+              type: TextAppType.bodyLarge,
+              color: AppColors.textAndIconPrimary,
+              fontWeight: FontWeight.w600,
+              fontSize: 20,
+            ),
+            SizedBox(height: 12.h),
+            TextApp(
+              text: 'ابدأ بإضافة رقم اللوحه ',
+              type: TextAppType.bodyMedium,
+              color: AppColors.textAndIconSecondary,
+              fontSize: 16,
+            ),
+          ],
         ),
       ),
     );
